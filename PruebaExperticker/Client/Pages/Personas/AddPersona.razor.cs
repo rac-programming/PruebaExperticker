e3dd32307b2882a30ed209f84d1b3b65a8909982
Rubén <94rac94@gmail.com>
2021-03-20 20:10:29 +0100

    Agregar archivos de proyecto.

---
+++ PruebaExperticker/Client/Pages/Personas/AddPersona.razor.cs
@@ -0,0 +1,34 @@
+﻿using PruebaExperticker.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace PruebaExperticker.Client.Pages.Personas
+{
+    public partial class AddPersona
+    {
+        public Persona persona = new Persona();
+        public string error;
+        public bool loading;
+
+        protected async Task Submit()
+        {
+            loading = true;
+            try
+            {
+                error = "";
+                var per = persona;
+                var res = await Http.PostAsJsonAsync("api/persona", per);
+                nav.NavigateTo("/personas");
+            }
+            catch
+            {
+                error = "Compruebe la conexión y vuelva a intentarlo";
+                this.StateHasChanged();
+            }
+            loading = false;
+        }
+    }
+}
---
+++ PruebaExperticker/Client/Pages/Personas/Personas.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using MatBlazor;
+using PruebaExperticker.Shared;
+
+namespace PruebaExperticker.Client.Pages.Personas
+{
+    public partial class Personas
+    {
+        [Inject]
+        protected IMatToaster Toaster { get; set; }
+
+        protected IEnumerable<Persona> personas { get; set; }
+
+ 
+        protected override async Task OnInitializedAsync()
+        {
+
+            //Al iniciar componente se hace petición a la API 
+            await FetchData();
+        }
+
+        protected async Task Borrar(string dni) {
+            try
+            {
+                await Http.DeleteAsync("api/persona/" + dni);
+                await FetchData();
+                StateHasChanged();
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected async Task FetchData() {
+            try
+            {
+                var res = await Http.GetFromJsonAsync<List<Persona>>("api/persona");
+                personas = res;
+            }
+            catch
+            {
+                Toaster.Add("Compruebe conexión y reintente", MatToastType.Danger, "Error", MatIconNames.Error);
+
+            }
+        }
+    }
+}
